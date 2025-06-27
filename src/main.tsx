2d76b31998f1d46e6c133f6ec85c7c3c73213f51
dhanuprys <dhanuprys@gmail.com>
2025-06-27 18:21:58 +0800

    feat: add vercel speed insights components

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import router from './router';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+    <StrictMode>
+        <RouterProvider router={router} />
+        <SpeedInsights />
+    </StrictMode>
 );
