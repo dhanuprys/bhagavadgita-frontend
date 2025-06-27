a987e5f419beaada2ce36e29fd1c3cac084e1e50
dhanuprys <dhanuprys@gmail.com>
2025-06-27 18:26:56 +0800

    hotfix: chagne next js analytics into react analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router';
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 import router from './router';
 
 createRoot(document.getElementById('root')!).render(
