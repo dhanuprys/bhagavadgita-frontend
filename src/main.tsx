6e85df3ee4cfd09bc9d0403a40bfe572f53a5ec7
dhanuprys <dhanuprys@gmail.com>
2025-06-27 18:23:09 +0800

    feat: add page analytics (by Vercel) into the page

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 import router from './router';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <RouterProvider router={router} />
         <SpeedInsights />
+        <Analytics />
     </StrictMode>
 );
